d5f4317b154ef1614b606b4b162c2df6cf546147
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-07-22 16:18:25 +0200

    feat: complete selection task

---
+++ 2-selection_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+size_t idx = 0, inner_idx = 0, min_pos = 0, tmp = 0;
+for (idx = 0; idx < size - 1; idx++)
+{
+min_pos = idx;
+for (inner_idx = idx + 1; inner_idx < size; inner_idx++)
+	if (array[inner_idx] < array[min_pos]) min_pos = inner_idx;
+
+if (min_pos != idx)
+{
+tmp = array[idx];
+array[idx] = array[min_pos];
+array[min_pos] = tmp;
+}
+
+print_array(array, size);
+}
+
+}
