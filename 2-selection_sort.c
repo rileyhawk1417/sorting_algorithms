caacd68e0f09a9ea49d19f1ca2734bc2c756ade7
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-07-25 17:37:23 +0200

    docs: add documentation for selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+/**
+ * selection_sort - entry point
+ * @array: the array to sort
+ * @size: size of the array
+ * Description: This uses selection_sort
+ * to sort the array elements
+ * Return: Nothing
+ */
 void selection_sort(int *array, size_t size)
 {
 size_t idx = 0, inner_idx = 0, min_pos = 0, tmp = 0;
@@ -7,7 +15,8 @@ for (idx = 0; idx < size - 1; idx++)
 {
 min_pos = idx;
 for (inner_idx = idx + 1; inner_idx < size; inner_idx++)
-	if (array[inner_idx] < array[min_pos]) min_pos = inner_idx;
+	if (array[inner_idx] < array[min_pos])
+		min_pos = inner_idx;
 
 if (min_pos != idx)
 {
