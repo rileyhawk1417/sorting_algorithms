ccefd09d69d73a600c839d4bb00774d30bd111e3
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-07-26 18:21:09 +0200

    fix: switch from do..while to for loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,19 +4,25 @@
 /**
 * bubble_sort - entry point
 * @array: the array to sort
-* @array_length: the length of the array
+* @size: the length of the array
 * Description: Uses the bubble sort algorithm
 * to swap values
 * Returns: Nothing
 */
-void bubble_sort(int *array, size_t array_length)
+void bubble_sort(int *array, size_t size)
 {
 size_t idx = 0, inner_idx = 0;
 int tmp = 0;
 bool swapped_value = false;
-do {
+
+if (array == NULL || size < 2)
+	return;
+
+for (idx = 0; idx < size - 1; idx++)
+{
+
 swapped_value = false;
-for (inner_idx = 0; inner_idx < (array_length - 1 - idx); inner_idx++)
+for (inner_idx = 0; inner_idx < (size - 1 - idx); inner_idx++)
 {
 	if (array[inner_idx] > array[inner_idx + 1])
 	{
@@ -24,10 +30,10 @@ for (inner_idx = 0; inner_idx < (array_length - 1 - idx); inner_idx++)
 		array[inner_idx] = array[inner_idx + 1];
 		array[inner_idx + 1] = tmp;
 		swapped_value = true;
+		print_array(array, size);
 	}
-	print_array(array, array_length);
 }
-	idx++;
-} while (swapped_value);
+
+} 
 
 }
